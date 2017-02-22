231d25502a94663d91605c82feec61a120ca64db
wijnandvancooten <wijnandvancooten@hotmail.com>
2017-02-22 15:22:30 +0100

    index and ejs

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express'),
       morgan = require('morgan'), //middle ware for console.log's in the terminal (debugging)
       bodyParser = require('body-parser'),
-      cookieSession = require('cookie-session');
+      cookieSession = require('cookie-session'),
+      ejs = require('ejs');
 
 
 var app = express();
 
-app.set('view engine', EJS);
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response)=> {
+  response.render('todo')
+});
+//server//
+app.listen(3000, function() {
+    console.log('Web server started on port 3000');
+});
